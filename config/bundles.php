6f353a24a30bb4c3193aaaccd925ff2876957f9f
77web <h-hishida@quartetcom.co.jp>
2017-12-04 13:58:17 +0900

    [composer] liip/functional-test-bundle nelmio/alice doctrine/data-fixtures doctrine/doctrine-fixtures-bundle hautelook/alice-bundle 追加

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,7 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+    Liip\FunctionalTestBundle\LiipFunctionalTestBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Hautelook\AliceBundle\HautelookAliceBundle::class => ['dev' => true, 'test' => true],
 ];
