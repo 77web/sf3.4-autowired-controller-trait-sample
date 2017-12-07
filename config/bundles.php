4d0a92ab7ebf21c9acadbb2a1db34f7d9b2a02d8
77web <info@77-web.com>
2017-12-07 11:02:06 +0900

    security-bundle, serializerを有効化

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Liip\FunctionalTestBundle\LiipFunctionalTestBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Hautelook\AliceBundle\HautelookAliceBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
